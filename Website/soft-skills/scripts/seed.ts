b7c13c2ffcaa3b72afd77f9f56d2559b362b8229
UnLogic <johannocuaman@gmail.com>
2025-04-07 23:38:38 -0700

    Database Schema & Lesson Buttons
    Implemented Database Schema & Lesson Buttons to showcase user progress and lesson plan

--- Website/soft-skills/scripts/seed.ts
+++ Website/soft-skills/scripts/seed.ts
@@ -14,6 +14,10 @@ const main = async () => {
 
         await db.delete(schema.courses);
         await db.delete(schema.userProgress);
+        await db.delete(schema.units);
+        await db.delete(schema.lessons);
+        await db.delete(schema.challengeOptions);
+        await db.delete(schema.challengeProgress);
 
         await db.insert(schema.courses).values([
             {
@@ -37,6 +41,86 @@ const main = async () => {
                 imageSrc: "/speaking.svg"
             }
         ]);
+
+        await db.insert(schema.units).values([
+            {
+                id: 1,
+                courseId: 1, // Conflict Resolution
+                title: "Unit 1",
+                description: "Learn the basics of Conflict Resolution",
+                order: 1,
+            },
+        ]);
+
+        await db.insert(schema.lessons).values([
+            {
+                id: 1,
+                unitId: 1, // Unit 1 (Learn the basics...)
+                order: 1,
+                title: "Listening",
+            },
+            {
+                id: 2,
+                unitId: 1, // Unit 1 (Learn the basics...)
+                order: 2,
+                title: "Feelings",
+            },
+            {
+                id: 3,
+                unitId: 1, // Unit 1 (Learn the basics...)
+                order: 3,
+                title: "Feelings",
+            },
+            {
+                id: 4,
+                unitId: 1, // Unit 1 (Learn the basics...)
+                order: 4,
+                title: "Feelings",
+            },
+            {
+                id: 5,
+                unitId: 1, // Unit 1 (Learn the basics...)
+                order: 5,
+                title: "Feelings",
+            },
+        ]);
+
+        await db.insert(schema.challenges).values([
+            {
+                id: 1,
+                lessonId: 1, // Listening
+                type: "SELECT",
+                order: 1,
+                question: 'Your friend Alex says: I\'m really stressed about the group project. I feel like I\'m doing all the work and no one else is helping.'
+            },
+        ])
+
+        await db.insert(schema.challengeOptions).values([
+            {
+                id: 1,
+                challengeId: 1,
+                //imageSrc: "/frustrated.svg",
+                correct: true,
+                text: "That sounds really frustrating. Do you want help talking to the group?",
+                //audioSrc: "come up with a name here lol.mp3",
+            },
+            {
+                id: 2,
+                challengeId: 1,
+                //imageSrc: "/frustrated.svg",
+                correct: false,
+                text: "You always complain about everything.",
+                //audioSrc: "come up with a name here lol.mp3",
+            },
+            {
+                id: 3,
+                challengeId: 1,
+                //imageSrc: "/frustrated.svg",
+                correct: false,
+                text: "You\'re probably just overreacting. It\'s not a big deal.",
+                //audioSrc: "come up with a name here lol.mp3",
+            },
+        ])
         
         console.log("Seeding finished");
     } catch (error) {
